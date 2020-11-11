4f4fc016cd9f4500c7f3ac2bdb1de660522c8bb3
ollyhearn <74230678+ollyhearn@users.noreply.github.com>
2020-11-11 06:48:51 +0000

    Update test000.cpp

--- test000.cpp
+++ test000.cpp
@@ -14,7 +14,7 @@ int main(){
 	init_pair(1, COLOR_BLACK, COLOR_WHITE);
 	init_pair(2, COLOR_YELLOW, COLOR_BLUE);
 	init_pair(3, COLOR_GREEN, COLOR_MAGENTA);
-//	while(h==h){
+	while(1){
 		while(temp<4){
 			move(0,0);
 			attron(COLOR_PAIR((int)temp));
@@ -23,7 +23,7 @@ int main(){
 			temp++;
 		}
 		temp=1;
-//	}
+	}
 	getch();
 	endwin();
 	return 0;
